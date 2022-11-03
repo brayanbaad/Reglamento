eb86dddbfd2d153e6413b92cc14755f80b382e0b
jangellopez <jangellopez@unicesar.edu.co>
2022-11-03 00:32:55 -0500

    diseño examen

--- lib/examens/examenCap2.dart
+++ lib/examens/examenCap2.dart
@@ -18,8 +18,109 @@ class _Examen2CapState extends State<Examen2Cap> {
         centerTitle: true,
         backgroundColor: Color.fromARGB(223, 240, 106, 16),
       ),
-      body: Center(
-        child: Text("Examen 2"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(25)),
+              child: Column(
+                children: [
+                  Text("¿Qué es la primera pregunta?"),
+                  Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Color.fromARGB(255, 255, 0, 0)),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Text(
+                        "A. Respuesta 1",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      Container(
+                        height: 26,
+                        width: 26,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(color: Color.fromARGB(255, 255, 0, 0))),
+                      )
+                    ]),
+                  ),
+                                    Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Text(
+                        "B. Respuesta 2",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      Container(
+                        height: 26,
+                        width: 26,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(color: Colors.grey)),
+                      )
+                    ]),
+                  ),
+                                    Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Color.fromARGB(255, 0, 201, 43)),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Text(
+                        "C. Respuesta 3",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      Container(
+                        height: 26,
+                        width: 26,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(color: Color.fromARGB(255, 0, 201, 43))),
+                      )
+                    ]),
+                  ),
+                                    Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Text(
+                        "D. Respuesta 4",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      Container(
+                        height: 26,
+                        width: 26,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(color: Colors.grey)),
+                      )
+                    ]),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
